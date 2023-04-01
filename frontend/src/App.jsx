eae38f06df02fc0efb6c05601fd874fbbf094f9d
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2023-03-31 20:12:06 -0400

    Changed from env to constant

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 // prefix for API URL
-const { API_PREFIX } = process.env ?? "http://localhost:4000";
+const { API_PREFIX } = "https://sea-turtle-app-qlvxz.ondigitalocean.app/api";
 
 function App() {
   const [texts, setTexts] = useState([]);
